4030b077591aacfcac6ab144b6db84927b443c5f
Raphaël Bonaventure <raph.bonaventure@gmail.com>
2018-02-28 23:30:59 +0100

    Create Google Analytics chart.

--- public/js/admin.js
+++ public/js/admin.js
@@ -30,8 +30,15 @@ $(function() {
 
   $("#update-ga").click(function() {
     const ga = $("#ga").val();
-    firebase.database().ref("/").update({ga_id: ga}).then(result => {
+    firebase.database().ref("/analytics").update({ga_id: ga}).then(result => {
       showSnackbar("L'ID Google Analytics a été mis à jour ave succès.")
+    }).catch(errorHandler);
+  });
+
+  $("#update-analytics-clientid").click(function() {
+    const token = $("#token").val();
+    firebase.database().ref("/analytics").update({ga_token: token}).then(result => {
+      showSnackbar("Le Client ID Google Analytics API a été mis à jour ave succès.")
     }).catch(errorHandler);
   });
 
@@ -69,7 +76,6 @@ $(function() {
   });
 
   refresh();
-  getBackups();
 })
 
 let showSnackbar = (message, handler) => {
@@ -86,11 +92,61 @@ let errorHandler = (error) => {
   showSnackbar('Une erreur est survenue, veuillez réessayer.');
 };
 
-let getBackups = () => {
-  firebase.database().ref("/cms/backups").once('value').then(data => {
-    
+let enableAnalyticsChart = (token) => {
+
+  gapi.analytics.ready(function() {
+
+    /**
+     * Authorize the user immediately if the user has already granted access.
+     * If no access has been created, render an authorize button inside the
+     * element with the ID "embed-api-auth-container".
+     */
+    gapi.analytics.auth.authorize({
+      container: 'embed-api-auth-container',
+      clientid: token
+    });
+  
+    /**
+     * Create a new ViewSelector instance to be rendered inside of an
+     * element with the id "view-selector-container".
+     */
+    var viewSelector = new gapi.analytics.ViewSelector({
+      container: 'view-selector-container'
+    });
+  
+    // Render the view selector to the page.
+    viewSelector.execute();
+  
+    /**
+     * Create a new DataChart instance with the given query parameters
+     * and Google chart options. It will be rendered inside an element
+     * with the id "chart-container".
+     */
+    var dataChart = new gapi.analytics.googleCharts.DataChart({
+      query: {
+        metrics: 'ga:sessions',
+        dimensions: 'ga:date',
+        'start-date': '30daysAgo',
+        'end-date': 'yesterday'
+      },
+      chart: {
+        container: 'chart-container',
+        type: 'LINE',
+        options: {
+          width: '100%'
+        }
+      }
+    });
+  
+    /**
+     * Render the dataChart on the page whenever a new view is selected.
+     */
+    viewSelector.on('change', function(ids) {
+      dataChart.set({query: {ids: ids}}).execute();
+    });
+  
   });
-}
+};
 
 let refresh = () => {
   
@@ -98,12 +154,17 @@ let refresh = () => {
     const site = data.val();
     $("#title").val(site.title);
     $("#description").val(site.description);
-    $("#ga").val(site.ga_id);
+    $("#ga").val(site.analytics.ga_id);
+    $("#token").val(site.analytics.ga_token);
     $("#facebook").val(site.socialmedia.facebook);
     $("#instagram").val(site.socialmedia.instagram);
     $("#credit").val(site.credit);
 
-    const list = $("#image-list");
+    if(site.analytics.ga_token) {
+      enableAnalyticsChart(site.analytics.ga_token);
+    }
+    
+    const list = $("#contents-list");
     list.empty();
     $.each(site.posts, function(i)
     {
